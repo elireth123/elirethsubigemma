f8d2b9cb15d97ce3bbee3fc27c7fffa4d68a59f6
Gemma Subirats <subigemma@gmail.com>
2017-04-23 20:15:11 +0200

    Creacion de tablas

---
+++ sql/RE_Atributos.sql
@@ -0,0 +1,12 @@
+CREATE TABLE RE_Attributos (
+  RE_pj varchar(120) NOT NULL DEFAULT '',
+  RE_Fisico int(10) unsigned NOT NULL DEFAULT '0',
+  RE_Destreza int(10) unsigned NOT NULL DEFAULT '0',
+  RE_Inteligencia int(10) unsigned NOT NULL DEFAULT '0',
+  RE_Percepcion int(10) unsigned NOT NULL DEFAULT '0',
+  RE_Mana int(10) unsigned NOT NULL DEFAULT '0',
+  RE_Vida tinyint(3) NOT NULL DEFAULT '0',
+  RE_Iniciativa int(10) unsigned NOT NULL DEFAULT '0',
+  RE_Defensa int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (RE_pj)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Atributos';
---
+++ sql/RE_Habilidades_pj.sql
@@ -0,0 +1,4 @@
+CREATE TABLE RE_Habilidades_pj (
+  RE_ID int(10) unsigned NOT NULL DEFAULT '0',
+  RE_pj varchar(120) NOT NULL DEFAULT '' 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Habilidades de pj';

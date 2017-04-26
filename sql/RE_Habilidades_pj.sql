1efd946ae1a3f485d7c400fb8f7d3b314f0cb101
Gemma Subirats Isern <subigemma@gmail.com>
2017-04-26 13:52:44 +0200

    Update RE_Habilidades_pj.sql

--- sql/RE_Habilidades_pj.sql
+++ sql/RE_Habilidades_pj.sql
@@ -1,4 +1,5 @@
-CREATE TABLE RE_Habilidades_pj (
-  RE_ID int(10) unsigned NOT NULL DEFAULT '0',
-  RE_pj varchar(120) NOT NULL DEFAULT '' 
+CREATE TABLE `RE_Habilidades_pj` (
+  `RE_ID` int(10) unsigned NOT NULL DEFAULT '0',
+  `RE_pj` varchar(120) NOT NULL DEFAULT '',
+  `RE_Valor` int(10) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Habilidades de pj';

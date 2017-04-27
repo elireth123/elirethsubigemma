bf977a517d782c333cdbfd037c30ad58b5a0bb33
Gemma Subirats Isern <subigemma@gmail.com>
2017-04-27 22:14:17 +0200

    Update RE_Habilidades.sql

--- sql/RE_Habilidades.sql
+++ sql/RE_Habilidades.sql
@@ -1,8 +1,9 @@
-CREATE TABLE `RE_Habilidades` (
-  `RE_ID` int(10) unsigned NOT NULL DEFAULT '0',
-  `RE_Nombre` varchar(120) NOT NULL DEFAULT '0',
-  `RE_Efecto` varchar(120) NOT NULL DEFAULT '',
-  `RE_Atributo` varchar(120) NOT NULL DEFAULT '',
-  `RE_Code` varchar(120) NOT NULL DEFAULT '',
-  PRIMARY KEY (`RE_ID`)
+CREATE TABLE RE_Habilidades (
+  RE_ID int(10) unsigned NOT NULL DEFAULT '0',
+  RE_Nombre varchar(120) NOT NULL DEFAULT '0',
+  RE_Efecto varchar(120) NOT NULL DEFAULT '',
+  RE_Atributo varchar(120) NOT NULL DEFAULT '',
+  RE_Code varchar(255) NOT NULL DEFAULT '',
+  RE_Mana int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (RE_ID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Habilidades';

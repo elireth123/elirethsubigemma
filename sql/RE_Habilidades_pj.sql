1dfb3d481b585453035891a9670c6bd81338d2f8
elireth123 <elireth123@hotmail.com>
2017-04-28 16:14:06 -0400

    Fail: PJ a minuscula como es debido y coma a su lugar
    Facepalm

--- sql/RE_Habilidades_pj.sql
+++ sql/RE_Habilidades_pj.sql
@@ -1,7 +1,7 @@
 CREATE TABLE RE_Habilidades_pj (
   RE_ID int(10) unsigned NOT NULL DEFAULT '0',
   RE_pj varchar(120) NOT NULL DEFAULT '',
-  RE_Valor int(10) NOT NULL DEFAULT '0'
+  RE_Valor int(10) NOT NULL DEFAULT '0',
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Habilidades de pj';
 
- ALTER TABLE RE_Habilidades_PJ ADD PRIMARY KEY ( RE_ID , RE_pj )  
+ ALTER TABLE RE_Habilidades_pj ADD PRIMARY KEY ( RE_ID , RE_pj )  
